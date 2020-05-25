776186c96d02db10927cde94ac441649127978d7
makinoshi <masa.kinoshita02@gmail.com>
2020-05-25 18:29:52 +0900

    Add metadata to server of Jetty9Server

--- src/toyokumo/commons/server/jetty9.clj
+++ src/toyokumo/commons/server/jetty9.clj
@@ -1,9 +1,12 @@
 (ns toyokumo.commons.server.jetty9
   (:require
    [com.stuartsierra.component :as component]
-   [ring.adapter.jetty9 :as jetty9]))
+   [ring.adapter.jetty9 :as jetty9])
+  (:import
+   (org.eclipse.jetty.server
+    Server)))
 
-(defrecord Jetty9Server [handler opts server]
+(defrecord Jetty9Server [handler opts ^Server server]
   component/Lifecycle
   (start [this]
     (if server

67b2c517d842dc5385c4b8c51e39557bfb009fa6
WiZLite <mikuto.matsuo@icloud.com>
2023-01-19 15:15:53 +0900

    Add docstring for toyokumo.commons.experimental.firebase.admin-sdk

--- src/toyokumo/commons/experimental/firebase/admin_sdk.clj
+++ src/toyokumo/commons/experimental/firebase/admin_sdk.clj
@@ -1,4 +1,11 @@
 (ns toyokumo.commons.experimental.firebase.admin-sdk
+  "Provides a component which initializes FirebasApp with the
+  `com.stuartsierra.component/Component` lifecycle.
+
+  service-account-key-path  - required. File path to the firebase service account key.
+  options-builder           - optional. An instance of FirebaseOptions$Builder. Use to customize FirebaseApp initialization.
+
+  See for more detail on Component at https://github.com/stuartsierra/component."
   (:require
    [clojure.java.io :as io]
    [com.stuartsierra.component :as component])

165d4404428c5bcd44d29282b6ecdc2b8e941a58
Rafael Kosaka <rafael.kosaka@toyokumo.co.jp>
2025-06-02 16:30:00 +0900

    add functions to convert data between clojure and ddb data format

---
+++ src/toyokumo/commons/aws/dynamo_db/format.clj
@@ -0,0 +1,36 @@
+(ns toyokumo.commons.aws.dynamo-db.format
+  (:require
+   [schema.core :as s])
+  (:import
+   (software.amazon.awssdk.services.dynamodb.model
+    AttributeValue)))
+
+(s/defn clj-string->ddb-string :- AttributeValue
+  "This function creates an AttributeValue with type String from a clojure String."
+  [value :- s/Str]
+  (-> (AttributeValue/builder)
+      (.s value)
+      (.build)))
+
+(s/defn ddb-string->clj-string :- s/Str
+  "This function converts an AttributeValue of type String to a clojure String."
+  [attribute-value :- AttributeValue]
+  (if-let [value (.s attribute-value)]
+    value
+    (throw (ex-info "AttributeValue is not type String" {}))))
+
+
+(s/defn clj-int->ddb-number :- AttributeValue
+  "This function creates an AttributeValue with type Number from a clojure Int."
+  [value :- s/Int]
+  (-> (AttributeValue/builder)
+      (.n (str value))
+      (.build)))
+
+(s/defn ddb-number->clj-int :- s/Int
+  "This function converts an AttributeValue of type Number to a clojure Int."
+  [attribute-value :- AttributeValue]
+  (if-let [value (.n attribute-value)]
+    (-> value
+        (Integer/parseInt))
+    (throw (ex-info "AttributeValue is not type Number" {}))))
---
+++ test/toyokumo/commons/aws/dynamo_db/format_test.clj
@@ -0,0 +1,41 @@
+(ns toyokumo.commons.aws.dynamo-db.format-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [toyokumo.commons.aws.dynamo-db.format :as sut])
+  (:import
+   (clojure.lang
+    ExceptionInfo)
+   (software.amazon.awssdk.services.dynamodb.model
+    AttributeValue)))
+
+(deftest clj-string->ddb-string-test
+  (is (= "dummy-string"
+         (-> (sut/clj-string->ddb-string "dummy-string")
+             (.s)))))
+
+(deftest ddb-string->clj-string-test
+  (is (= "dummy-string"
+         (sut/ddb-string->clj-string
+          (sut/clj-string->ddb-string "dummy-string"))))
+  (is (thrown? ExceptionInfo
+        (sut/ddb-string->clj-string
+         (-> (AttributeValue/builder)
+             (.n "1")
+             (.build))))
+      "When passing an AttributeValue that is not of type String, an exception is thrown."))
+
+(deftest clj-int->ddb-number-test
+  (is (= "100"
+         (-> (sut/clj-int->ddb-number 100)
+             (.n)))))
+
+(deftest ddb-number->clj-int-test
+  (is (= 100
+         (sut/ddb-number->clj-int
+          (sut/clj-int->ddb-number 100)))
+      (is (thrown? ExceptionInfo
+            (sut/ddb-number->clj-int
+             (-> (AttributeValue/builder)
+                 (.s "dummy-string")
+                 (.build))))
+          "When passing an AttributeValue that is not of type Number, an exception is thrown.")))

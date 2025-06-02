74151ae1613c43dc8be159b017fdb8c5a8916c9a
Rafael Kosaka <rafael.kosaka@toyokumo.co.jp>
2025-06-02 16:32:25 +0900

    add dynamo db component

---
+++ src/toyokumo/commons/aws/dynamo_db/component.clj
@@ -0,0 +1,35 @@
+(ns toyokumo.commons.aws.dynamo-db.component
+  (:require
+   [com.stuartsierra.component :as component]
+   [schema.core :as s])
+  (:import
+   (java.net
+    URI)
+   (software.amazon.awssdk.auth.credentials
+    AwsCredentialsProvider)
+   (software.amazon.awssdk.regions
+    Region)
+   (software.amazon.awssdk.services.dynamodb
+    DynamoDbClient)))
+
+(s/defrecord AwsDynamoDb
+  [dynamo-db-client :- DynamoDbClient
+   region :- (s/maybe Region)
+   endpoint :- (s/maybe URI)
+   credentials :- (s/maybe AwsCredentialsProvider)]
+
+  component/Lifecycle
+  (start [this]
+    (let [client (-> (DynamoDbClient/builder)
+                     (cond->
+                      region (.region region)
+                      endpoint (.endpointOverride endpoint)
+                      credentials (.credentialsProvider credentials))
+                     (.build))]
+      (println "DynamoDB client created" client)
+      (assoc this
+             :client client)))
+  (stop [this]
+    (when-let [client (:client this)]
+      (.close client))
+    (assoc this :client nil)))

8e0610f28274dcdb946da0b5020c0aa829f717d6
Rafael Kosaka <rafael.kosaka@toyokumo.co.jp>
2025-06-03 15:49:51 +0900

    create partition key inside get-item

--- src/toyokumo/commons/aws/dynamo_db/request.clj
+++ src/toyokumo/commons/aws/dynamo_db/request.clj
@@ -9,7 +9,18 @@
     AttributeValue
     GetItemRequest)))
 
+(s/defschema PartitionKeyInfo {:name s/Str
+                               :value s/Str})
+
 (s/defschema PartitionKey {s/Str AttributeValue})
+
+(s/defn ^:private create-partition-key :- {s/Str AttributeValue}
+  "Creates a partition key map for DynamoDB.
+   The key is the partition key name and the value is the attribute value."
+  [{:keys [:name :value]} :- PartitionKeyInfo]
+  {name (-> (AttributeValue/builder)
+            (.s value)
+            (.build))})
 
 (s/defschema DynamoDbItem {s/Str (s/cond-pre s/Int s/Str s/Bool nil)})
 
@@ -27,10 +38,10 @@
    Returns a map of attribute names to AttributeValue, or nil if the item does not exist."
   [client :- DynamoDbClient
    table-name :- s/Str
-   partition-key :- PartitionKey]
+   partition-key-info :- PartitionKeyInfo]
   (let [get-item-res (.getItem client
                                (build-get-item-request table-name
-                                                       partition-key))]
+                                                       (create-partition-key partition-key-info)))]
     (when (.hasItem get-item-res)
       (-> (.item get-item-res)
           ;; convert Map<String, AttributeValue> to clojure map

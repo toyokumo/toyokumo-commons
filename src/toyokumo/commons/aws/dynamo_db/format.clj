c58c43edb9e1a429479cc02fa682ad0ea6a7f57d
Rafael Kosaka <rafael.kosaka@toyokumo.co.jp>
2025-06-03 15:36:58 +0900

    improve format function

--- src/toyokumo/commons/aws/dynamo_db/format.clj
+++ src/toyokumo/commons/aws/dynamo_db/format.clj
@@ -5,32 +5,43 @@
    (software.amazon.awssdk.services.dynamodb.model
     AttributeValue)))
 
-(s/defn clj-string->ddb-string :- AttributeValue
-  "This function creates an AttributeValue with type String from a clojure String."
-  [value :- s/Str]
-  (-> (AttributeValue/builder)
-      (.s value)
-      (.build)))
+;; (s/defn clj-string->ddb-string :- AttributeValue
+;;   "This function creates an AttributeValue with type String from a clojure String."
+;;   [value :- s/Str]
+;;   (-> (AttributeValue/builder)
+;;       (.s value)
+;;       (.build)))
 
-(s/defn ddb-string->clj-string :- s/Str
-  "This function converts an AttributeValue of type String to a clojure String."
+;; (s/defn ddb-string->clj-string :- s/Str
+;;   "This function converts an AttributeValue of type String to a clojure String."
+;;   [attribute-value :- AttributeValue]
+;;   (if-let [value (.s attribute-value)]
+;;     value
+;;     (throw (ex-info "AttributeValue is not type String" {}))))
+
+
+;; (s/defn clj-int->ddb-number :- AttributeValue
+;;   "This function creates an AttributeValue with type Number from a clojure Int."
+;;   [value :- s/Int]
+;;   (-> (AttributeValue/builder)
+;;       (.n (str value))
+;;       (.build)))
+
+;; (s/defn ddb-number->clj-int :- s/Int
+;;   "This function converts an AttributeValue of type Number to a clojure Int."
+;;   [attribute-value :- AttributeValue]
+;;   (if-let [value (.n attribute-value)]
+;;     (-> value
+;;         (Integer/parseInt))
+;;     (throw (ex-info "AttributeValue is not type Number" {}))))
+
+
+(s/defn attribute-value->clj
+  "Converts an AttributeValue to a clojure data structure.
+   This function supports String, Number, Boolean, and Null types."
   [attribute-value :- AttributeValue]
-  (if-let [value (.s attribute-value)]
-    value
-    (throw (ex-info "AttributeValue is not type String" {}))))
-
-
-(s/defn clj-int->ddb-number :- AttributeValue
-  "This function creates an AttributeValue with type Number from a clojure Int."
-  [value :- s/Int]
-  (-> (AttributeValue/builder)
-      (.n (str value))
-      (.build)))
-
-(s/defn ddb-number->clj-int :- s/Int
-  "This function converts an AttributeValue of type Number to a clojure Int."
-  [attribute-value :- AttributeValue]
-  (if-let [value (.n attribute-value)]
-    (-> value
-        (Integer/parseInt))
-    (throw (ex-info "AttributeValue is not type Number" {}))))
+  (cond
+    (some? (.s attribute-value)) (.s attribute-value)
+    (some? (.n attribute-value)) (Integer/parseInt (.n attribute-value))
+    (some? (.bool attribute-value)) (.bool attribute-value)
+    :else nil))
